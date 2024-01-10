1c8d9e1af8be2d32d175019a1d36551cbca13661
Ali <asaad@snelstart.nl>
2024-01-10 13:25:49 +0100

    First project commit

---
+++ src/main/java/com/example/serviceconnect/ServiceConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConnectApplication.class, args);
+	}
+
+}
